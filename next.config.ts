d3360c56b6b02ce23adab8f297cbe16815fe14cc
Gn0rl <gnrltv4@gmail.com>
2024-10-22 19:35:08 +0400

    upload

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    sassOptions: {
+      implementation: 'sass-embedded',
+    },
+    i18n: {
+      // These are all the locales you want to support in
+      // your application
+      locales: ['en-US', 'fr', 'nl-NL'],
+      // This is the default locale you want to be used when visiting
+      // a non-locale prefixed path e.g. `/hello`
+      defaultLocale: 'ru',
+    },
 };
 
 export default nextConfig;
